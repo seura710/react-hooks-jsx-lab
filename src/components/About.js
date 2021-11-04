f94808bc83a05619f348e837db8e1fb7a953b67c
Harris Kim <seura710@gmail.com>
2021-11-04 01:44:28 -0400

    commit tree 00ab100d96301a6e681ab909d60bb71724023425

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,21 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I am a fullstack developer</p>
+    <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this"/>
+  </div>;
 }
 
 export default About;
+
+
+
+{/* // The <About> component should return the following JSX elements:
+
+// a <div> element with an id of "about"
+// inside the div element, there should be three child elements:
+// an <h2> element with the content of "About Me"
+// a <p> element with any content of your choosing
+// an <img> element with the src attribute set to the image variable, and the alt attribute set to "I made this" */}
